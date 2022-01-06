6c8595cbf3774e42f4614e81dd8c6ea40f32a64c
Kalicia <d00372177@dmail.dixie.edu>
2022-01-05 19:02:50 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+hamburger_handler()
+function hamburger_handler() {
+    var menu = document.querySelector(".hamburger-menu")
+    var content = document.querySelector(".items")
+    menu.classList.toggle('toggle');
+    content.classList.toggle('toggle');
+
+}
